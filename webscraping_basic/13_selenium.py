aa16410a90cadb8f88a24547f752b68db15c7a1f
A540 <51090700+A540@users.noreply.github.com>
2022-11-06 15:41:56 +0900

    Feat 13_selenium.py
    왓챠 로그인

--- webscraping_basic/13_selenium.py
+++ webscraping_basic/13_selenium.py
@@ -1,3 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-browser = webdriver.Chrome("./chromedriver.exe")
+
+browser = webdriver.Chrome()
+browser.get("https://watcha.com/")
+
+elem = browser.find_element(By.XPATH,"//*[@id='root']/div[1]/nav/ul[2]/li/a")
+elem.click()
+
+browser.find_element(By.XPATH,"//*[@id='root']/div[1]/main/div/main/div/form/div[1]/input").send_keys("개인id")
+browser.find_element(By.XPATH,"//*[@id='root']/div[1]/main/div/main/div/form/div[2]/input").send_keys("개인pw")
+
+browser.find_element(By.XPATH,"//*[@id='root']/div[1]/main/div/main/div/form/div[3]/button").click()
+
+time.sleep(2)
+
+browser.find_element(By.CLASS_NAME,"css-1blyq76").click()
+
+while(True):
+    pass
